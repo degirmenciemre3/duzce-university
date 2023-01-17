44bf4674d8510e61edf80f872ea84bfee82e4f44
Elif Sinem AĞAR <65136162+sinemagar@users.noreply.github.com>
2023-01-18 00:55:46 +0300

    Update api.js

--- src/components/apis/api.js
+++ src/components/apis/api.js
@@ -1,5 +1,4 @@
 export const announcementURL = "https://duzceuniversitywebapi.azurewebsites.net/api/Duyurular/DuyurularGet";
-<<<<<<< HEAD
 export const broadcastURL = "https://63885fc3d94a7e5040998c96.mockapi.io/yayinlar";
 export const AccountLogin = "https://duzceuniversitywebapi20221227225156.azurewebsites.net/api/Account/AccountLogin";
 export const AdminDuyuruApi = "https://duzceuniversitywebapi20221227225156.azurewebsites.net/api/Duyurular/DuyurularGet"
